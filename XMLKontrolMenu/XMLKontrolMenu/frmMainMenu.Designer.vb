c1384391a626e134748dc22b85280b103a95d863
emirkarpuzmegayazilim <emirkarpuz.megayazilim@outlook.com>
2021-02-10 15:37:02 +0300

    TAG Okuma yapıldı

--- XMLKontrolMenu/XMLKontrolMenu/frmMainMenu.Designer.vb
+++ XMLKontrolMenu/XMLKontrolMenu/frmMainMenu.Designer.vb
@@ -22,17 +22,32 @@ Partial Class frmMainMenu
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtXML = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'txtXML
+        '
+        Me.txtXML.Location = New System.Drawing.Point(12, 12)
+        Me.txtXML.Multiline = True
+        Me.txtXML.Name = "txtXML"
+        Me.txtXML.ReadOnly = True
+        Me.txtXML.Size = New System.Drawing.Size(426, 463)
+        Me.txtXML.TabIndex = 0
         '
         'frmMainMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(450, 487)
+        Me.Controls.Add(Me.txtXML)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "frmMainMenu"
         Me.Text = "XML Kontrolleri"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents txtXML As TextBox
 End Class
--- XMLKontrolMenu/XMLKontrolMenu/frmMainMenu.vb
+++ XMLKontrolMenu/XMLKontrolMenu/frmMainMenu.vb
@@ -1,3 +1,26 @@
-﻿Public Class frmMainMenu
+﻿Imports System.Xml
 
+Public Class frmMainMenu
+    Dim xmlText As XmlReader = XmlReader.Create(Application.StartupPath & "\test.xml")
+
+    Public Sub readXML()
+        While xmlText.Read()
+            If xmlText.IsStartElement() Then
+                Select Case xmlText.Name.ToString
+                    Case "to"
+                        txtXML.Text += xmlText.ReadElementContentAsString & Environment.NewLine
+                    Case "from"
+                        txtXML.Text += xmlText.ReadElementContentAsString & Environment.NewLine
+                    Case "heading"
+                        txtXML.Text += xmlText.ReadElementContentAsString & Environment.NewLine
+                    Case "body"
+                        txtXML.Text += xmlText.ReadElementContentAsString & Environment.NewLine
+                End Select
+            End If
+        End While
+    End Sub
+
+    Private Sub frmMainMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        readXML()
+    End Sub
 End Class
